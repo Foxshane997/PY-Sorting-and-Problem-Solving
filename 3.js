9a63a3c35d8f47421b8b141d7776a7c9d06728fb
“Foxshane997” <Foxshane997@gmail.com>
2024-09-04 20:02:03 -0700

    Finished algorithm 3

--- 3.js
+++ 3.js
@@ -1,1 +1,15 @@
 // Write your solution for algorithm 3 below
+function sortListDescending(unsortedList) {
+    let sortedList = [...unsortedList];
+
+    sortedList.sort((a, b) => a - b);
+    
+    sortedList.reverse();
+
+    return sortedList;
+}
+
+const unsortedList = [5, 3, 8, 1, 2];
+const sortedDescendingList = sortListDescending(unsortedList);
+
+console.log("Sorted Copied Numerical List Descending:", sortedDescendingList);
