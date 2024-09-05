8e403ce37107b3033653b39f7b0c63b472811fd9
“Foxshane997” <Foxshane997@gmail.com>
2024-09-04 19:48:41 -0700

    Finished algorithm 2

--- 2.js
+++ 2.js
@@ -1,1 +1,17 @@
 // Write your solution for algorithm 2 below
+
+function sortNumericalList(unsortedList){ 
+    // Copying the List/Array
+    let sortedList = [...unsortedList];
+
+    // Ascending order
+    sortedList.sort((a,b)=> a - b);
+
+    return sortedList
+}
+
+const unsortedList = [5, 3, 8, 1, 2]
+const sortedList = sortNumericalList(unsortedList)
+
+console.log("Copy Sorted List", sortedList)
+console.log('Original List', unsortedList)
